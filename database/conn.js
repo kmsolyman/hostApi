893b3b9a36238b78f676885678a49d7c5120068f
kmsolyman <134826118+kmsolyman@users.noreply.github.com>
2024-01-30 14:25:55 +0600

    Update conn.js
    ok

--- database/conn.js
+++ database/conn.js
@@ -1,24 +1,14 @@
 const mongoose = require('mongoose');
-// const DB = process.env.DATABASE ;
 
-// mongoose.connect(DB,{
-//     useNewUrlParser:true,
-//     useCreateIndex:true,
-//     useUnifiedTopology:true,
-//     useFindAndModify:false
-// }).then(()=>{
-//     console.log('connect successfully db')
-// }).catch((err)=> console.log('no connection db massage'));
-
-const DB ="mongodb+srv://kmsolyman:solyman@cluster0.7wpalig.mongodb.net/token?retryWrites=true&w=majority";
+const DATABASE ="mongodb+srv://kmsolyman:solyman@cluster0.7wpalig.mongodb.net/token?retryWrites=true&w=majority";
  
-mongoose.connect(DB,
-  // {
-  //  useNewUrlParser:true,
+mongoose.connect(DATABASE,
+  {
+   useNewUrlParser:true,
   //  useCreateIndex:true,
-  //  useUnifiedTopology:true,
+   useUnifiedTopology:true,
   //  useFindAndModify:false
-  // }
+  }
   )
   .then(()=>{console.log("connect successfull mongose");
 }).catch((err)=> console.log('no connect mongose'));
